40bb76eabda0570a40c04b8907a700d8979493bb
rgdev-03 <grpdev.03@gmail.com>
2024-02-24 13:41:58 +0530

    commited

--- src/pages/Student/AddSemMarks.tsx
+++ src/pages/Student/AddSemMarks.tsx
@@ -1,4 +1,4 @@
-import { NavBar } from '@/components/NavBar/NavBar';
+import { NavBar } from '@/components/Navbar/Navbar';
 import { AppShell, Burger, Group, Skeleton, Title,Text, Box,Avatar,Card,Table, TableData, Button,Center,TextInput,Container, Grid,Mark,Dialog} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './student.module.css'
